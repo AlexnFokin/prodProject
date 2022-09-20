ed90abed5b6ad6b5883a87ab01f1073bdeb7f885
a-fokin <a.fokin@sxope.com>
2022-09-20 17:49:23 +0300

    added theme switcher widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,14 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProviders";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 const App = () => {
 
-  const {theme, toggleTheme} = useTheme()
+  const {theme} = useTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>toggle theme</button>
+<ThemeSwitcher/>
      <Navbar/>
       <AppRouter/>
     </div>
