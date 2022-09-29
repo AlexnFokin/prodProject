5abdc6f06158f810ef364e01b67000c63f2b4546
a-fokin <a.fokin@sxope.com>
2022-09-29 19:57:20 +0300

    added testing storybook & loki

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,17 @@ module.exports = {
     es2021: true,
     jest: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     "project": ["tsconfig.json"],
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    "i18next"
-  ],
+  plugins: ['react', '@typescript-eslint', "i18next"],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
@@ -40,18 +33,18 @@ module.exports = {
     'no-underscore-dangle': 'off',
     "@typescript-eslint/strict-boolean-expressions": "warn",
     "@typescript-eslint/explicit-function-return-type": "warn",
-    "i18next/no-literal-string": [
-      'error',
-      {
-        markupOnly: true,
-        ignoreAttribute: ['data-testid', 'to']
-      }
-    ],
-    "max-len": ['error', {"ignoreComments": true, code: 100}],
+    "i18next/no-literal-string": ['error', {
+      markupOnly: true,
+      ignoreAttribute: ['data-testid', 'to']
+    }],
+    "max-len": ['error', {
+      "ignoreComments": true,
+      code: 100
+    }],
     "@typescript-eslint/naming-convention": "warn"
   },
   globals: {
-    __IS_DEV__: true,
+    __IS_DEV__: true
   },
   overrides: [
     {
