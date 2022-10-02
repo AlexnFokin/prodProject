3d2fc324d3b3e5d1e4ef1d55dba58a2ab27c43ac
a-fokin <a.fokin@sxope.com>
2022-10-02 20:31:22 +0300

    disable testing

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,7 +20,7 @@ export function buildPlugins (
 
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin())
-    plugins.push(new BundleAnalyzerPlugin())
+    // plugins.push(new BundleAnalyzerPlugin())
   }
 
   return plugins
